f37059a883bbf58fc5edc7b33fc494552f8ab186
Byron Encinas-Velazquez <byron_encinas@outlook.com>
2026-04-04 04:51:28 +0000

    New test for python interop

--- python/Toolkit.py
+++ python/Toolkit.py
@@ -2,10 +2,11 @@ import ctypes, os, sys
 
 class Toolkit:
     def __init__(self):
-        lib_path = os.path.join(os.getcwd(), "build/libcpt.so")
-        self.tools = ctypes.CDLL(lib_path)
+        self.lib_path = os.path.join(os.getcwd(), "../build/libcpt.so")
+        self.tools = ctypes.CDLL(self.lib_path)
 
     def random_between(self, l, u):
+        # it uses the same seed all the time, not a good function
         self.tools.rand_within.argtypes = [ctypes.c_float, ctypes.c_float]
         self.tools.rand_within.restype  = ctypes.c_float
         return self.tools.rand_within(l, u)
---
+++ python/main.py
@@ -0,0 +1,5 @@
+from Toolkit import Toolkit
+
+instance = Toolkit()
+
+print(instance.random_between(0.0, 1.0))
