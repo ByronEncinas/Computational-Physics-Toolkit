8ef3bbccb38a804cb3220f31c8b4f947a8054e7f
carajava32 <crjavierv97@gmail.com>
2026-04-11 00:38:59 -0500

    I am not sure if I did things right. I copied the code from the main and pasted it into the files that had conflicts. Please check if I did things correctly.

--- include/rk4.h
+++ include/rk4.h
@@ -1,6 +1,7 @@
 #ifndef RK4
 #define RK4
 
+<<<<<<< HEAD
 #include <stdio.h>
 #include <stddef.h>
 #include <math.h>
@@ -10,5 +11,6 @@ float function(float *matrix1, float *matrix2, int len, int eqs, float *consts);
 float matrix_var(float *matrix, float var1, float var2, int len);
 float runge_kutta_o4(float h,float *matrix1, int eqs,int len, float *consts);
 void solution(float *matrix, float total_time, float h, float number, float *consts);
+
 
 #endif
