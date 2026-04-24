520d82dee40a7b5f8d2e43fd64d836c8f686424a
carajava32 <crjavierv97@gmail.com>
2026-04-24 00:37:22 -0500

    With Byron we did some changes on the mesh_test.c file.

--- tests/mesh_test.c
+++ tests/mesh_test.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../include/toolkit.h"
+#include "../include/geom.h"
+#include "../include/imesh.h"
+#include "../include/mesh.h"
+
 
 const int hull_size = 53;
 const float hull[] = {
